39300f1e6f5acfce16c1bb62f56cd00db9c7b125
MestreLion <linux@rodrigosilva.com>
2024-12-20 00:25:04 -0300

    refactor: decouple main logic from rendering

--- src/main/java/com/rodrigosilva/SimpleInfoHUD.java
+++ src/main/java/com/rodrigosilva/SimpleInfoHUD.java
@@ -13,20 +13,28 @@ public class SimpleInfoHUD implements ModInitializer {
 	public static final String MOD_ID = "simple-info-hud";
 	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 	public static final MinecraftClient CLIENT = MinecraftClient.getInstance();
+	public static MatrixStack MATRIX_STACK;
 
 	@Override
 	public void onInitialize() {
-		HudRenderCallback.EVENT.register(SimpleInfoHUD::renderSimpleInfoHUD);
+		// Callback is (DrawContext context, float tickDelta) in Minecraft 1.20+
+		// https://fabricmc.net/2023/05/25/120.html#screen-and-rendering-changes
+		HudRenderCallback.EVENT.register((matrices, tickDelta) -> {
+			MATRIX_STACK = matrices;
+			mainSimpleInfoHUD();
+		});
 		LOGGER.info("[SimpleInfoHUD] Initialized");
 	}
 
-	// Called as (DrawContext context, float tickDelta) in Minecraft 1.20+
-	// https://fabricmc.net/2023/05/25/120.html#screen-and-rendering-changes
-	public static final void renderSimpleInfoHUD(MatrixStack matrices, float tickDelta) {
+	public static final void mainSimpleInfoHUD() {
 		float msgX = 2;
 		float msgY = 2;
 		int color = 0xFFFFFFFF;  // White
 		String msg = "Hello SimpleInfoHUD!";
-		CLIENT.textRenderer.drawWithShadow(matrices, msg, msgX, msgY, color);
+		displayHud(msg, msgX, msgY, color);
+	}
+
+	public static void displayHud(String msg, float x, float y, int color) {
+		CLIENT.textRenderer.drawWithShadow(MATRIX_STACK, msg, x, y, color);
 	}
 }
